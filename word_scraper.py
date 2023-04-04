008f1dc71ebf35bef0e88cf15511c984e0400af1
IshanC05 <ishaanchugh05@gmail.com>
2023-04-04 12:57:09 +0530

    Fixed bugs with GUI

--- word_scraper.py
+++ word_scraper.py
@@ -1,11 +1,13 @@
-#Import the libraries
+# Import the libraries
 import requests
-from   bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-#This function scrapes the website merriam-webster for the word of the day
+# This function scrapes the website merriam-webster for the word of the day
+
+
 def today_s_word():
-    url  = 'https://www.merriam-webster.com/word-of-the-day'
-    r    = requests.get(url)
+    url = 'https://www.merriam-webster.com/word-of-the-day'
+    r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     word = soup.h2.text
     meaning = soup.find('p').get_text()
